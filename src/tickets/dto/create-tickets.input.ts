35835a2236acc5f53dd75064ea66244da2baf90c
Krishnagopal Sinha <krishnagopalsinha@Krishnagopals-MacBook-Pro.local>
2023-12-07 08:20:15 +0530

    minor fix

--- src/tickets/dto/create-tickets.input.ts
+++ src/tickets/dto/create-tickets.input.ts
@@ -1,23 +1,30 @@
 import { InputType, Int, Field, registerEnumType } from '@nestjs/graphql';
+import { ApiProperty } from '@nestjs/swagger';
 import { Priority, Status } from '@prisma/client';
 
 @InputType()
 export class CreateTicketsInput {
   @Field(() => Int)
+  @ApiProperty()
   userID: number;
 
   @Field()
+  @ApiProperty()
   subject: string;
 
   @Field()
+  @ApiProperty()
   description: string;
 
   @Field(() => Status,{ nullable: true })
-  status: Status;
+  @ApiProperty({required: false})
+  status?: Status;
 
   @Field(() => Priority, { nullable: true })
+  @ApiProperty({required: false})
   priority?: Priority;
 
   @Field(() => Int, { nullable: true })
+  @ApiProperty({required: false})
   assignedTo?: number;
 }
--- src/tickets/dto/update-tickets.input.ts
+++ src/tickets/dto/update-tickets.input.ts
@@ -2,32 +2,37 @@
 import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
 import { CreateTicketsInput } from './create-tickets.input';
 import { Priority, Status } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
 
 @InputType()
 export class UpdateTicketslInput extends PartialType(
   CreateTicketsInput,
 ) {
-  // @Field(() => Int)
-  // ticketID: number;
+  @Field(() => Int,{ nullable: true })
+  @ApiProperty({required: false})
+  userID?: number;
 
-  @Field(() => Int)
-  userID: number;
+  @Field({ nullable: true })
+  @ApiProperty({required: false})
+  subject?: string;
 
-  @Field()
-  subject: string;
+  @Field({ nullable: true })
+  @ApiProperty({required: false})
+  description?: string;
 
-  @Field()
-  description: string;
-
-  @Field(() => Status)
-  status: Status;
+  @Field(() => Status,{ nullable: true })
+  @ApiProperty({required: false})
+  status?: Status;
 
   @Field(() => Priority, { nullable: true })
+  @ApiProperty({required: false})
   priority?: Priority;
 
   @Field(() => Int, { nullable: true })
+  @ApiProperty({required: false})
   assignedTo?: number;
 
   @Field(() => Date, { nullable: true })
+  @ApiProperty({required: false})
   closedAt?: Date;
 }
